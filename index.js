576488796b3fce368852bc947a02f7a260256fb8
footballjones <footballjones308@gmail.com>
2023-03-16 22:58:59 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(name) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push (`Thank you, ${name[i]}, for the wonderful surprise gift!`)  
+    }
+    return messages
+}
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber)
+        positiveNumber--
+    }
+}
